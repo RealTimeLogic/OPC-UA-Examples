f6c77a6abb9747b86a3963f42af9585e24f3f5df
Alexander Rykovanov <alexander@realtimelogic.com>
2021-03-13 01:59:32 +0300

    Supporting multichunk messages.

--- mako.sh
+++ mako.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 path=$(dirname $0)
-LUA_CPATH="${BAROOT}/src/opcua/opcua_ns0/?.so;${path}/?.so" MAKO_ZIP=${BAROOT}/examples/MakoServer/obj/release/mako.zip ${BAROOT}/examples/MakoServer/obj/release/mako -c ${path}/mako.conf.dev
+MAKO_ZIP=${BAROOT}/examples/MakoServer/obj/release/mako.zip ${BAROOT}/examples/MakoServer/obj/release/mako -c ${path}/mako.conf.dev
 
