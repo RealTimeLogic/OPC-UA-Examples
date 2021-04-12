b6056f539b2ec693bcdc55739ef3b4ef28805c11
Alexander Rykovanov <alexander@realtimelogic.com>
2021-04-13 01:19:01 +0300

    Start minimum server.

--- server/1_start/start_server.lua
+++ server/1_start/start_server.lua
@@ -1,49 +1,11 @@
 -- Load an OPCUA API
 local ua = require("opcua.api")
 
--- Create server configuration table
-
-local config = {
-  -- listenPort=4841,
-  -- listenAddress="*",
-  -- endpointUrl = "opc.tcp://loclahost:4841"
-
-  listenPort=4841,
-  listenAddress="*",
-  endpointUrl="opc.tcp://localhost:4841",
-
-  securityPolicyUri = ua.Types.SecurityPolicy.None,
-  bufSize = 16384,
-  logging = {
-    socket = {
-      dbgOn = true,  -- debug logs of socket
-      infOn = true,  -- information logs about sockets
-      errOn = true   -- Errors on sockets
-    },
-    binary = {
-      dbgOn = true,  -- Debugging traces about binary protocol. Print encoded message hex data.
-      infOn = true,  -- Information traces about binary protocol
-      errOn = true   -- Errors in binary protocol
-    },
-    services = {
-      dbgOn = true,  -- Debugging traces about UA services work
-      infOn = true,  -- Informations traces
-      errOn = true   -- Errors
-    }
-  }
-
-}
-
 -- New instance of an OPC UA server
-local server = ua.newServer(config)
+local server = ua.newServer()
 
--- Initialize and load addons if addon function provided.
-server:initialize(DemoAddon)
+-- Initialize server.
+server:initialize()
 
--- Run server. Starts to listen ports
+-- Run server. Start listening to ports
 server:run()
-
--- Server starts asynchroniously so we can do another (un)usefull work.
-while true do
-    ba.sleep(1000)
-end
