30ad2911215c440a81fb540f2bf0a31d522ffd98
Alexander Rykovanov <rykovanov.as@gmail.com>
2021-04-14 02:25:11 +0300

    Example of configuring server

---
+++ server/2_server_config/opcua_server.lua
@@ -0,0 +1,18 @@
+-- Load an OPCUA API
+local ua = require("opcua.api")
+
+local config = {
+   listenPort = 4841,
+   listenAddress = "localhost", -- '*' to listen on all interfaces
+   endpointUrl = "opc.tcp://localhost:4841" -- Endpoint URL is used by clients to establish secure channel.
+} 
+
+-- New instance of an OPC UA server
+-- Pass configuration table to server.
+local server = ua.newServer(config)
+
+-- Initialize server.
+server:initialize()
+
+-- Run server. Start listening to port
+server:run()
