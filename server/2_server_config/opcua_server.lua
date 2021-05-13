b1493b19eaea9715e89437438c7d8e6390c9723c
Alexander Rykovanov <alexander@realtimelogic.com>
2021-05-13 09:33:18 +0300

    Example 2_server_config: Added logging settings to config.

--- server/2_server_config/opcua_server.lua
+++ server/2_server_config/opcua_server.lua
@@ -4,7 +4,25 @@ local ua = require("opcua.api")
 local config = {
    listenPort = 4841,
    listenAddress = "localhost", -- '*' to listen on all interfaces
-   endpointUrl = "opc.tcp://localhost:4841" -- Endpoint URL is used by clients to establish secure channel.
+   endpointUrl = "opc.tcp://localhost:4841", -- Endpoint URL is used by clients to establish secure channel.
+
+   logging = {
+    socket = {
+      dbgOn = true,
+      infOn = true,
+      errOn = true,
+    },
+    binary = {
+      dbgOn = true,
+      infOn = true,
+      errOn = true,
+    },
+    services = {
+      dbgOn = false,
+      infOn = true,
+      errOn = true,
+    }
+  }
 }
 
 -- New instance of an OPC UA server
